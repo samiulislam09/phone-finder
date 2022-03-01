58c6c94a07e175f46918322393cffe43642059e1
samiulislam09 <samiulislamsawon09@gmail.com>
2022-03-01 14:28:53 +0600

    added button

--- js/script.js
+++ js/script.js
@@ -1,34 +1,37 @@
+document.getElementById('loading').style.display = "none";
+// fetch data
 const phoneData = () => {
     const inputFromuser = document.getElementById('name-input').value;
     const phoneName = inputFromuser.toLowerCase();
-    toggleLoading('block')
     fetch(`https://openapi.programming-hero.com/api/phones?search=${phoneName}`)
         .then(res => res.json())
         .then(data => displayPhone(data))
-    toggleNotFound('none');
+    toggleLoading('block')
+    document.getElementById('not-found').style.display = "none";
 }
-
+// not found handler
 const toggleNotFound = (input) => {
     const notFound = document.getElementById('not-found');
     notFound.style.display = input;
     
 }
-
+// loading handler
 const toggleLoading = (input) => {
     const loading = document.getElementById('loading');
     loading.style.display = input;
 }
-
+// displaying data
 const displayPhone = (data) => {
-    const cards = document.getElementById('cards');
-    cards.textContent = '';
-    toggleNotFound('none')
-    
     if (data.status == false) {
-        toggleNotFound('block');
-        toggleLoading('none');
+        cards.textContent = "";
+        document.getElementById('not-found').style.display = 'block';
+        toggleLoading('none')
     } else {
-        for (let item of data.data) {
+        const cards = document.getElementById('cards');
+        cards.textContent = "";
+        const allData = data.data;
+        const firstData = data.data.slice(0, 20);
+        for (let item of firstData) {
             const div = document.createElement('div');
             div.classList.add('col-12');
             div.classList.add('col-md-3');
@@ -36,19 +39,21 @@ const displayPhone = (data) => {
             div.classList.add('justify-content-center');
             div.classList.add('text-center');
             div.innerHTML = `
-                    <div class="card g-2" style="width: 90%;">
-                        <img src="${item.image}" class="card-img-top" alt="${item.brand} mobile image">
-                        <div class="card-body">
-                            <h5 class="card-title">${item.brand}</h5>
-                            <p class="card-text">${item.phone_name}</p>
-                        </div>
+                <div class="card g-2" style="width: 90%;">
+                    <img src="${item.image}" class="card-img-top" alt="${item.brand} mobile image">
+                    <div class="card-body">
+                        <h5 class="card-title">${item.brand}</h5>
+                        <p class="card-text">${item.phone_name}</p>
+                        <button class="btn btn-primary">Specification</button>
                     </div>
-        `
+                </div>
+    `
             cards.appendChild(div)
         }
-        toggleLoading('none')
+        
+        
+        toggleNotFound('none');
+        toggleLoading('none');
     }
     
-
-
 }
